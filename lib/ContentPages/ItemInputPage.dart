5daa1947df4b02c718e653d6c8fa5cd79d266a99
KuhakuPixel <Nicholaspixels@gmail.com>
2021-01-21 06:07:56 +0700

    Base class for itemInputPage

---
+++ lib/ContentPages/ItemInputPage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ItemInputPageWidget extends StatefulWidget {
+  final String inputPageTitle;
+  //not including spacing, needs to create spacing directly
+  final List<Widget> inputWidgetListImplementation;
+  @override
+  _ItemInputPageWidgetState createState() => _ItemInputPageWidgetState();
+  ItemInputPageWidget({
+    this.inputPageTitle,
+    this.inputWidgetListImplementation,
+  });
+}
+
+class _ItemInputPageWidgetState extends State<ItemInputPageWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.inputPageTitle),
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          //widgets for this page(containing search field,boxed container and ect)
+          child: Column(
+            children: widget.inputWidgetListImplementation,
+          ),
+        ),
+        alignment: Alignment.topLeft,
+      ),
+    );
+  }
+}
