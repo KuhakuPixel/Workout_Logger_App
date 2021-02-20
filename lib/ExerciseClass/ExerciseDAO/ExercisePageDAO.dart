4250dc8b758e73f451ccdab06b495be0d42f8d19
KuhakuPixel <66545911+KuhakuPixel@users.noreply.github.com>
2021-02-20 21:46:38 +0700

    Update ExercisePageDAO.dart

--- lib/ExerciseClass/ExerciseDAO/ExercisePageDAO.dart
+++ lib/ExerciseClass/ExerciseDAO/ExercisePageDAO.dart
@@ -16,9 +16,4 @@ class ExercisePageDAO {
   factory ExercisePageDAO.fromJson(Map<String, dynamic> json) => _$ExercisePageDAOFromJson(json);
   Map<String, dynamic> toJson() => _$ExercisePageDAOToJson(this);
 }
-//problem definition:in what readable pattern can we save the map by turning it to string
-//turn the signature of tojson to flutter pub run build_runner buildstring
-//explicitcly convert an object of class type to its json string before storing it inside a map :create a function to save and load the data map (converting to jsonString first)
-//create a fucntion to load and save the dao? inside the dao class itself
 
-//have fun
